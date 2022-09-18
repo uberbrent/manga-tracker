5edbb779ced4ff4862f3a8226efa02427eb51d08
uberbrent <bmjohns1337@gmail.com>
2022-09-17 22:21:33 -0500

    commiting initial files

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { HashRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <h1>Hello World</h1>
+  );
+}
+
+function WrappedApp() {
+  return (
+    <HashRouter>
+      <App />
+    </HashRouter>
+  );
+}
+
+export default WrappedApp;
